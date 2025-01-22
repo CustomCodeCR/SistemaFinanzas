c5a5a356e307aaa108feacc716c315c96f4b45f6
MauLang18 <maulangbonilla.18@gmail.com>
2025-01-21 23:09:36 -0600

    Initial commit

---
+++ config/app.php
@@ -0,0 +1,7 @@
+<?php
+
+    const APP_URL="http://localhost/SistemaFinanzas/";
+    const APP_NAME="Prueba";
+    const APP_SESSION_NAME="SF";
+    
+    date_default_timezone_set("America/Costa_Rica");
