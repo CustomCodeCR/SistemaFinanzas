f04b6e734b933b1e213e89e910b0a5ada76a8b23
Lativ24 <avillalobos60841@ufide.ac.cr>
2025-03-07 13:08:17 -0600

    fix: incluyendo un texto para movimientos

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -56,6 +56,7 @@
             <li>
                 <a href="#submenu2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                     <i class="fas fa-exchange-alt"></i> 
+                    <span>Movimientos</span>
                     <i class="fas fa-angle-down"></i> 
                 </a>
                 <ul class="collapse list-unstyled" id="submenu2">
